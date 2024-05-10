f7713bc09f8bbffd6dc9ff34395f3d696c3cd191
Jackson Mangallay <jacksonmdev@outlook.com>
2024-05-10 18:46:15 +0800

    chat message

---
+++ src/components/MessageItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div style="width: 100%">
+    <q-chat-message
+      name="Jane"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      :text="['hey, Can I ask something?']"
+    />
+    <q-chat-message
+      name="me"
+      avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+      :text="['hey, how are you?']"
+      sent
+    />
+    <q-chat-message
+      name="Jane"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      :text="['doing fine, how r you?']"
+    />
+    <q-chat-message
+      v-for="(chat, index) in chats"
+      :key="index"
+      name="me"
+      avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+      :text="[`${chat}`]"
+      sent
+    />
+  </div>
+  <div style="width: 100%" class="row q-mt-md">
+    <div class="col-11">
+      <form-input label="Type your message..." v-model="chat"></form-input>
+    </div>
+    <div class="col-1">
+      <q-btn
+        square
+        color="secondary"
+        icon="send"
+        style="height: 56px"
+        flat
+        @click="sendChat"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import FormInput from './FormInput.vue';
+
+const chat = ref<string>();
+const chats = ref<string[]>([]);
+
+const sendChat = () => {
+  if (chat.value) {
+    chats.value?.push(chat.value);
+    chat.value = '';
+  }
+};
+</script>
+
+<style scoped></style>
--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -1,7 +1,10 @@
 <template>
   <q-page class="row justify-evenly q-pa-md">
     <div class="col-12" v-for="(message, index) in messages" :key="index">
-      <q-card class="q-mb-md message-wrapper">
+      <q-card
+        class="q-mb-md message-wrapper"
+        @click="showActionDialog(`${message.user}`, MessageItem)"
+      >
         <q-item class="q-py-lg">
           <q-item-section side>
             <q-avatar circle size="48px">
@@ -16,11 +19,25 @@
         </q-item>
       </q-card>
     </div>
+
+    <action-dialog
+      :show="toggleDialog"
+      :title="toggleDialogTitle"
+      @update:model-value="toggleDialog = false"
+    >
+      <component :is="toggleDialogContent"></component>
+    </action-dialog>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, shallowRef } from 'vue';
+import ActionDialog from 'src/components/ActionDialog.vue';
+import MessageItem from 'src/components/MessageItem.vue';
+
+const toggleDialog = ref(false);
+const toggleDialogTitle = ref('');
+const toggleDialogContent = shallowRef();
 
 const messages = ref([
   {
@@ -36,6 +53,12 @@ const messages = ref([
     preview: 'What breed?',
   },
 ]);
+
+const showActionDialog = (title: string, component: unknown) => {
+  toggleDialogTitle.value = title;
+  toggleDialogContent.value = component;
+  toggleDialog.value = true;
+};
 </script>
 
 <style scoped>
