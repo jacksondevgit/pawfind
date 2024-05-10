4a56dcd9b55daab1a9040e1049a13c549a87ad57
Jackson Mangallay <jacksonmdev@outlook.com>
2024-05-10 18:15:43 +0800

    initial message

--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -1,24 +1,20 @@
 <template>
   <q-page class="row justify-evenly q-pa-md">
-    <div class="row">
-      <div
-        class="col-12"
-        v-for="(notification, index) in notifications"
-        :key="index"
-      >
-        <q-card class="q-mb-md">
-          <q-card-section>
-            <q-btn
-              round
-              flat
-              :color="iconColor(notification.icon)"
-              :icon="notification.icon"
-              class="q-mr-md"
-            />
-            {{ notification.content }}
-          </q-card-section>
-        </q-card>
-      </div>
+    <div class="col-12" v-for="(message, index) in messages" :key="index">
+      <q-card class="q-mb-md message-wrapper">
+        <q-item class="q-py-lg">
+          <q-item-section side>
+            <q-avatar circle size="48px">
+              <img src="https://cdn.quasar.dev/img/avatar.png" />
+              <!-- <q-badge floating color="teal">new</q-badge> -->
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ message.user }}</q-item-label>
+            <q-item-label caption>{{ message.preview }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-card>
     </div>
   </q-page>
 </template>
@@ -26,52 +22,24 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const notifications = ref([
+const messages = ref([
   {
-    icon: 'mood',
-    type: 'ask',
-    content:
-      'Chen has a question. Can you share your insight? Join the conversation now!',
+    user: 'Chen',
+    preview: 'Want to adopt',
   },
   {
-    icon: 'info',
-    type: 'inform',
-    content: 'Chen shared an information. Check it now!',
+    user: 'Andrew',
+    preview: 'Pa adopt',
   },
   {
-    icon: 'pets',
-    type: 'adopt',
-    content:
-      "Looking to adopt? Check out the newest fury friend up for adoption. They might just be the companion you've been searching for!",
-  },
-  {
-    icon: 'campaign',
-    type: 'announcement',
-    content:
-      "Attention: An important update has been shared by the authorities. Don't miss our the latest information. View details now",
-  },
-  {
-    icon: 'error',
-    type: 'rescue',
-    content:
-      'Rescue Alert: A dog in your area needs immediate help! Your quick response could save a life. Act now!',
+    user: 'Nerie',
+    preview: 'What breed?',
   },
 ]);
-
-const iconColor = (icon: string) => {
-  switch (icon) {
-    case 'mood':
-      return 'yellow';
-    case 'info':
-      return 'blue';
-    case 'pets':
-      return 'orange';
-    case 'campaign':
-      return 'grey';
-    case 'error':
-      return 'red';
-  }
-};
 </script>
 
-<style scoped></style>
+<style scoped>
+.message-wrapper {
+  min-width: 400px;
+}
+</style>
