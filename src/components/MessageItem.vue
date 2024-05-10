8f61e47c74344eb9c393f38fd508ad261ddca450
Jackson Mangallay <jacksonmdev@outlook.com>
2024-05-10 18:50:26 +0800

    sample auto reply

--- src/components/MessageItem.vue
+++ src/components/MessageItem.vue
@@ -19,10 +19,12 @@
     <q-chat-message
       v-for="(chat, index) in chats"
       :key="index"
-      name="me"
-      avatar="https://cdn.quasar.dev/img/avatar1.jpg"
-      :text="[`${chat}`]"
-      sent
+      :name="chat.sent ? `me` : 'Jane'"
+      :avatar="`https://cdn.quasar.dev/img/${
+        chat.sent ? 'avatar1' : 'avatar2'
+      }.jpg`"
+      :text="[`${chat.message}`]"
+      :sent="chat.sent"
     />
   </div>
   <div style="width: 100%" class="row q-mt-md">
@@ -47,11 +49,16 @@ import { ref } from 'vue';
 import FormInput from './FormInput.vue';
 
 const chat = ref<string>();
-const chats = ref<string[]>([]);
+const chats = ref<Array<{ message: string; sent: boolean }>>([]);
 
 const sendChat = () => {
   if (chat.value) {
-    chats.value?.push(chat.value);
+    chats.value?.push({ message: chat.value, sent: true });
+    chats.value?.push({
+      message: 'Sorry, I have some unfinished tasks yet',
+      sent: false,
+    });
+
     chat.value = '';
   }
 };
